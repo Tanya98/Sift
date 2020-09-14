8658c46dd4971f548e090cad9a42958c36dd96cf
tatyana290498 <29tatyna04@gmail.com>
2020-09-14 15:00:38 +0300

    feat: added submit methods

--- src/app/main/search/search.component.ts
+++ src/app/main/search/search.component.ts
@@ -1,20 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, OnChanges, DoCheck, AfterContentChecked, AfterContentInit } from '@angular/core';
 import { faSearch, faTimes, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import { SendInputTextService } from 'src/app/services/send-input-text.service';
+import { TextInputModel } from 'src/app/model/text-input-model';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
-  selector: 'main-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.less']
+    selector: 'main-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.less']
 })
-export class SearchComponent implements OnInit {
-  faSearch = faSearch;
-  faTimes = faTimes;
-  faEllipsisH = faEllipsisH;
-  closeSearch = 1;
+export class SearchComponent implements OnInit, OnDestroy, AfterContentInit {
+    faSearch = faSearch;
+    faTimes = faTimes;
+    faEllipsisH = faEllipsisH;
+    closeSearch = 1;
 
-  constructor(){}
+    public data;
+    private _destroy$: Subject<void> = new Subject();
 
-  ngOnInit() {
-  }
+    constructor(private service: SendInputTextService) { }
+
+    ngOnInit() {
+    }
+
+    ngAfterContentInit() {
+        this.service.inputResponse$.subscribe(res=> console.log(res));
+    }
+
+    public enterSubmit(input) {
+        this.submit(input.value);
+        input.value = null;
+    }
+
+    public submit(value) {
+        const model: TextInputModel = {
+            clientId: 60,
+            language: 'en',
+            text: value,
+            contentType: 'SHORT_TEXT'
+        }
+        this.service.sendInputText(model).pipe(
+            takeUntil(this._destroy$))
+            .subscribe(res => this.service.inputResponse$.next(res));
+    }
+
+    ngOnDestroy(): void {
+        this._destroy$.next();
+        this._destroy$.complete();
+    }
 
 }
--- src/app/services/send-input-text.service.ts
+++ src/app/services/send-input-text.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { TextInputModel } from '../model/text-input-model';
-import { BehaviorSubject } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class SendInputTextService {
@@ -10,7 +10,7 @@ export class SendInputTextService {
     constructor(private http: HttpClient) { }
     private url = environment.apiBaseUrl;
 
-    public subject: BehaviorSubject<any> = new BehaviorSubject<any>('');
+    public inputResponse$: Subject<any> = new Subject<any>();
 
     public sendInputText(model: TextInputModel): any {
         return this.http.post(`${this.url}/classify/text`, model);
